86b1a59386b2c1f02d7c81e14bb02622fd846927
Fabio <fabio@fabiofarias.com.br>
2020-08-24 00:27:02 -0300

    CAPROVER

---
+++ build/precache-manifest.dd878ffe4bad8ae9927eb3a25f9fcc4e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597ffb3ac9e54d9ce32c92eded5efb16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d081fd10784597904c8f",
+    "url": "/static/css/main.9e206d81.chunk.css"
+  },
+  {
+    "revision": "f2a57424b78b6f73ccf3",
+    "url": "/static/js/2.6d8f00c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d8f00c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d081fd10784597904c8f",
+    "url": "/static/js/main.8d5818b0.chunk.js"
+  },
+  {
+    "revision": "ef80ed9a0ab6307d7e61",
+    "url": "/static/js/runtime-main.ea75e96f.js"
+  },
+  {
+    "revision": "dc8c47fa945838a208074157d997436d",
+    "url": "/static/media/back.dc8c47fa.svg"
+  },
+  {
+    "revision": "156102ee75a899123483211bbf4a8fbd",
+    "url": "/static/media/give-classes.156102ee.svg"
+  },
+  {
+    "revision": "211f2bb00860f91804cfb2354e17b255",
+    "url": "/static/media/landing.211f2bb0.svg"
+  },
+  {
+    "revision": "8ce7222d6c1116359d1f4bbb545e6060",
+    "url": "/static/media/logo.8ce7222d.jpg"
+  },
+  {
+    "revision": "be4de42cb4990e2c5c2cede6e1ca55ff",
+    "url": "/static/media/logo.be4de42c.svg"
+  },
+  {
+    "revision": "bfdb3ebe513b5b2411a5014d6aad3abe",
+    "url": "/static/media/purple-heart.bfdb3ebe.svg"
+  },
+  {
+    "revision": "e7a7e1b4e016245fa97e6f42aee4bcaa",
+    "url": "/static/media/study.e7a7e1b4.svg"
+  },
+  {
+    "revision": "6dfaa2f94fc54e53666a511b779f90b4",
+    "url": "/static/media/warning.6dfaa2f9.svg"
+  },
+  {
+    "revision": "45c6e6ecebde5ee441644d41cc132ce4",
+    "url": "/static/media/whatsapp.45c6e6ec.svg"
+  }
+]);
